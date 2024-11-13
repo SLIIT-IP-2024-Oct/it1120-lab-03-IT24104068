dd26a9ee7d7acbefe37eb2f4bd9a194f75902af7
IT24104068 <it24104068@my.sliit.lk>
2024-11-14 01:32:48 +0530

    Add files via upload

---
+++ Q1A/IT24104068Lab3Q1A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IT24104068Lab3Q1A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the price of 1 kg of rice: ");
+        double pricePerKg = scanner.nextDouble();
+        
+        System.out.print("Enter the number of kilograms you want to buy: ");
+        double kilograms = scanner.nextDouble();
+        
+        double totalAmount = pricePerKg * kilograms;
+        
+        System.out.println("The total amount is: " + totalAmount);
+        
+        scanner.close();
+    }
+}
